de51334efb62b2e63edaeb6b3f570fbe99d99b01
Adam Baczkowski <s20132@pjwstk.edu.pl>
2021-03-11 09:44:38 +0100

    dodałem nowy projekt

---
+++ PJATK/Crawler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Crawler //jeśli klasy są w tej samej przestrzeni to siebie widzą
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpClient httpClient = new HttpClient();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
